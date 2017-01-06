c66c29e9a85a5bc0ce077334750af945689865e9
Noah Berman <noah@noahberman.org>
2017-01-05 21:17:08 -0700

    server switch and minor page edit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,4 @@ class PagesController < ApplicationController
   def index  
   end
   
-  def letsencrypt
-      render text: "g_Lg14RVF4xJoUvUP6JimuDArzpqe27MF2waw2fQhrc.y7J190Z88fdVtXgvwJMMQ1FLPA66Hbymzt_juuyzoUY"
-  end
-  
 end
