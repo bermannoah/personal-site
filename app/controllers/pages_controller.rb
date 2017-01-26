d34f908c1bc5e76238c6a0667bd449d3eb672b34
Noah Berman <noah@noahberman.org>
2017-01-26 09:42:52 -0700

    fix resume and add link to s3 version

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,4 +6,7 @@ class PagesController < ApplicationController
   def portfolio
   end
   
+  def resume
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
   root "pages#index"
-  get "/portfolio", to: "pages#portfolio"
 
 end
